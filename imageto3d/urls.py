966b4042b980527145021aeac645fa9663347e2a
Mayank Prasoon <mayank.prasoon@outlook.com>
2020-09-19 21:43:52 +0530

    Azure constants addition

--- imageto3d/urls.py
+++ imageto3d/urls.py
@@ -1,7 +1,10 @@
 
+from django.urls import path
 from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
 router.register(r"imageto3d", Imageto3dViewset)
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('get_image_3d/', Imageto3dAPI.as_view())
+]
